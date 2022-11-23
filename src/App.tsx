9f3c379b2bf48a077dd5d333c521b09aacfc2679
kanon chakma <kanon121chakma@gmail.com>
2022-11-23 23:03:48 +0600

    added routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,55 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import NavBar from './component/NavBar/NavBar';
+import {
+  createBrowserRouter,
+  Navigate,
+  Outlet,
+  RouterProvider,
+} from "react-router-dom";
+
+import Login from './component/Login/Login';
+import { Register } from './component/Register/Register';
+import Home from './pages/home/Home';
 
 function App() {
+
+  const Layout = () => {
+    return (
+      <div>
+        <NavBar />
+        <div style={{ display: "flex" }}>
+          <Outlet />
+        </div>
+      </div>
+    );
+  };
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: (
+          <Layout />
+      ),
+      children: [
+        {
+          path: "/",
+          element: <Home />,
+        },
+      ],
+    },
+    {
+      path: "/login",
+      element: <Login />,
+    },
+    {
+      path: "/register",
+      element: <Register />,
+    },
+  ]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <RouterProvider router={router} />
+    </>
   );
 }
 
---
+++ src/component/NavBar/NavBar.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const NavBar = () => {
+  return (
+    <div className="navbar">
+        <div className="navconainer">
+            <span className="log">Booking Hotel</span>
+            <div className="navItems">
+                <button className="navButton">Register</button>
+                <button className="navButton">Login</button>
+            </div>
+        </div>
+
+    </div>
+  )
+}
+
+export default NavBar
